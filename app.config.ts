9cfa7f65a57abfb9a302520c9cc8d24e7a1fc3eb
kealymmb <kealymmb@gmail.com>
2022-10-14 08:17:44 +0200

    Added inline babel plugin

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,16 @@ export default () => ({
   expo: {
     extra: {
       ACCESS_KEY: process.env.ACCESS_KEY || null,
+      eas: {
+        projectId: "173dad65-c6bf-4cb3-b742-ab2b4afc249d",
+      },
+    },
+    runtimeVersion: {
+      policy: "sdkVersion",
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+      url: "https://u.expo.dev/173dad65-c6bf-4cb3-b742-ab2b4afc249d",
     },
   },
 });
