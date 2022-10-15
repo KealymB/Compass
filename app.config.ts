d5937428ec3551c601e83d469fdf94a0daec18f1
kealymmb <kealymmb@gmail.com>
2022-10-15 09:44:17 +0200

    Removed access key

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,7 @@
 export default () => ({
   expo: {
     extra: {
-      ACCESS_KEY:
-        process.env.ACCESS_KEY || "Xy4u4hVMFAQSKO0sRY57zq5vtzw10wwpPPqTz0ejL7Q",
+      ACCESS_KEY: process.env.ACCESS_KEY || null,
       eas: {
         projectId: "173dad65-c6bf-4cb3-b742-ab2b4afc249d",
       },
